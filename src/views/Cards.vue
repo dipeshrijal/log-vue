216d0dc5423edffa3e8d5694bb611cec6c4be6b7
Dipesh Rijal <dipesh.rijal@gmail.com>
2020-12-24 04:12:13 -0600

    filter css & html

--- src/views/Cards.vue
+++ src/views/Cards.vue
@@ -1,135 +1,297 @@
 <template>
   <div class="container-fluid">
     <!-- Page Heading -->
-    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-      <h1 class="h4 mb-0 text-gray-800">Tickers ({{ stocks.length }})</h1>
-
-      <!-- Nav Item - User Information -->
-      <span class="dropdown">
-        <a
-          @click="toggleFilterDropdown"
-          class="d-none d-sm-inline-block float-end btn btn-sm btn-info shadow-sm"
-          id="filterDropdown"
-          role="button"
-          data-toggle="dropdown"
-          aria-haspopup="true"
-          aria-expanded="false"
-          ><i class="fas fa-filter fa-sm text-white-50"></i> {{ filter }}
-        </a>
-        <!-- Dropdown - User Information -->
-        <div
-          class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
-          :class="filterDropdown ? 'show' : ''"
-          aria-labelledby="filterDropdown"
-        >
-          <a class="dropdown-item" @click="all('All')">
-            <i class="fas fa-balance-scale fa-sm fa-fw mr-2 text-gray-400"></i>
-            All
-          </a>
-          <a class="dropdown-item" @click="profit('Profit')">
-            <i class="fas fa-level-up-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-            Profit
-          </a>
-
-          <a class="dropdown-item" @click="loss('Loss')">
-            <i class="fas fa-level-down-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-            Loss
-          </a>
-
-          <a class="dropdown-item" @click="open('Open')">
-            <i class="fas fa-money-check fa-sm fa-fw mr-2 text-gray-400"></i>
-            Open
-          </a>
-
-          <div class="dropdown-divider"></div>
-          <a
-            class="dropdown-item"
-            @click="recent(1, 'Last 1 Day')"
-            data-toggle="modal"
-            data-target="#uploadModal"
-          >
-            <i class="fas fa-calendar fa-sm fa-fw mr-2 text-gray-400"></i>
-            Last 1 Day
-          </a>
-          <a
-            class="dropdown-item"
-            @click="recent(3, 'Last 3 Days')"
-            data-toggle="modal"
-            data-target="#uploadModal"
-          >
-            <i class="fas fa-calendar fa-sm fa-fw mr-2 text-gray-400"></i>
-            Last 3 Days
-          </a>
-          <a
-            class="dropdown-item"
-            @click="recent(7, 'Last 7 Days')"
-            data-toggle="modal"
-            data-target="#uploadModal"
-          >
-            <i class="fas fa-calendar fa-sm fa-fw mr-2 text-gray-400"></i>
-            Last 15 Days
-          </a>
-          <a
-            class="dropdown-item"
-            @click="recent(7, 'Last 15 Days')"
-            data-toggle="modal"
-            data-target="#uploadModal"
-          >
-            <i class="fas fa-calendar fa-sm fa-fw mr-2 text-gray-400"></i>
-            Last 15 Days
-          </a>
-          <a
-            class="dropdown-item"
-            @click="recent(30, 'Last 30 Days')"
-            data-toggle="modal"
-            data-target="#uploadModal"
-          >
-            <i class="fas fa-calendar fa-sm fa-fw mr-2 text-gray-400"></i>
-            Last 30 Days
-          </a>
-        </div>
-      </span>
-    </div>
     <div class="row">
-      <div
-        class="col-xl-2 col-md-6 mb-4"
-        v-for="stock in stocks"
-        :key="stock._id"
-      >
-        <div
-          class="card shadow h-100 py-2"
-          :class="
-            stock.total >= 0 ? 'border-left-success' : 'border-left-danger'
-          "
-        >
-          <!-- <router-link :to="{ name: 'Tables', params: { id: 'fb' } }"> -->
-          <router-link :to="{ name: 'Tables', params: { id: stock._id } }">
-            <div class="card-body">
-              <div class="row no-gutters align-items-center">
-                <div class="col mr-2">
-                  <div
-                    class="text-xs font-weight-bold text-uppercase mb-1"
-                    :class="stock.total >= 0 ? 'text-success' : 'text-danger'"
-                  >
-                    ${{ stock.total }}
-                  </div>
-                  <div
-                    class="h7 mb-0 font-weight-bold"
-                    :class="stock.total >= 0 ? 'text-success' : 'text-danger'"
-                  >
-                    {{ stock._id }}
-                  </div>
-                </div>
-                <!-- <div class="col-auto">
+      <div class="col-md-2">
+        <section>
+          <!-- Section: Filters -->
+          <section class="filters">
+            <h5>Filters</h5>
+            <!-- Section: Condition -->
+            <section filter="condition" class="mb-4">
+              <h6 class="font-weight-bold mb-3">Condition</h6>
+
+              <div class="form-check pl-0 mb-3">
+                <input
+                  type="checkbox"
+                  class="filter-option form-check-input filled-in"
+                  @click="open('Open')"
+                  condition="new"
+                  id="new"
+                />
+                <label
+                  class="form-check-label small text-uppercase card-link-secondary"
+                  for="new"
+                  >Open</label
+                >
+              </div>
+              <div class="form-check pl-0 mb-3">
+                <input
+                  type="checkbox"
+                  class="filter-option form-check-input filled-in"
+                  @click="open('Open')"
+                  condition="closed"
+                  id="closed"
+                />
+                <label
+                  class="form-check-label small text-uppercase card-link-secondary"
+                  for="closed"
+                  >closed</label
+                >
+              </div>
+            </section>
+            <!-- Section: Condition -->
+
+            <!-- Section: Price -->
+            <section class="mb-4" filter="price">
+              <h6 class="font-weight-bold mb-3">Price</h6>
+
+              <div class="form-check pl-0 mb-3">
+                <input
+                  price="0-25"
+                  type="radio"
+                  @click="all('All')"
+                  class="filter-option form-check-input"
+                  id="under25"
+                  name="materialExampleRadios"
+                />
+                <label
+                  class="form-check-label small text-uppercase card-link-secondary"
+                  for="under25"
+                  >All</label
+                >
+              </div>
+              <div class="form-check pl-0 mb-3">
+                <input
+                  price="25-50"
+                  type="radio"
+                  class="filter-option form-check-input"
+                  id="2550"
+                  @click="profit('Profit')"
+                  name="materialExampleRadios"
+                />
+                <label
+                  class="form-check-label small text-uppercase card-link-secondary"
+                  for="2550"
+                  >Profit</label
+                >
+              </div>
+              <div class="form-check pl-0 mb-3">
+                <input
+                  price="50-100"
+                  type="radio"
+                  @click="loss('loss')"
+                  class="filter-option form-check-input"
+                  id="50100"
+                  name="materialExampleRadios"
+                />
+                <label
+                  class="form-check-label small text-uppercase card-link-secondary"
+                  for="50100"
+                  >Loss</label
+                >
+              </div>
+            </section>
+            <!-- Section: Price -->
+            <!-- Section: Timeline -->
+            <section class="mb-4" filter="price">
+              <h6 class="font-weight-bold mb-3">Time Frame</h6>
+
+              <div class="form-check pl-0 mb-3">
+                <input
+                  price="0-25"
+                  type="radio"
+                  @click="recent(7, 'Last 1 Day')"
+                  class="filter-option form-check-input"
+                  id="last1day"
+                  name="materialExampleRadios1"
+                />
+                <label
+                  class="form-check-label small text-uppercase card-link-secondary"
+                  for="last1day"
+                  >Last 7 Day</label
+                >
+              </div>
+              <div class="form-check pl-0 mb-3">
+                <input
+                  price="25-50"
+                  type="radio"
+                  class="filter-option form-check-input"
+                  id="2550"
+                  @click="profit('Profit')"
+                  name="materialExampleRadios1"
+                />
+                <label
+                  class="form-check-label small text-uppercase card-link-secondary"
+                  for="2550"
+                  >Profit</label
+                >
+              </div>
+              <div class="form-check pl-0 mb-3">
+                <input
+                  price="50-100"
+                  type="radio"
+                  @click="loss('loss')"
+                  class="filter-option form-check-input"
+                  id="50100"
+                  name="materialExampleRadios1"
+                />
+                <label
+                  class="form-check-label small text-uppercase card-link-secondary"
+                  for="50100"
+                  >Loss</label
+                >
+              </div>
+            </section>
+            <!-- Section: Timeline -->
+          </section>
+          <!-- Section: Filters -->
+        </section>
+      </div>
+      <div class="col-md-10">
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+          <h1 class="h4 mb-0 text-gray-800">Tickers ({{ stocks.length }})</h1>
+
+          <!-- Nav Item - User Information -->
+          <span class="dropdown">
+            <a
+              @click="toggleFilterDropdown"
+              class="d-none d-sm-inline-block float-end btn btn-sm btn-info shadow-sm"
+              id="filterDropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+              ><i class="fas fa-filter fa-sm text-white-50"></i> {{ filter }}
+            </a>
+            <!-- Dropdown - User Information -->
+            <div
+              class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+              :class="filterDropdown ? 'show' : ''"
+              aria-labelledby="filterDropdown"
+            >
+              <a class="dropdown-item" @click="all('All')">
+                <i
+                  class="fas fa-balance-scale fa-sm fa-fw mr-2 text-gray-400"
+                ></i>
+                All
+              </a>
+              <a class="dropdown-item" @click="profit('Profit')">
+                <i
+                  class="fas fa-level-up-alt fa-sm fa-fw mr-2 text-gray-400"
+                ></i>
+                Profit
+              </a>
+
+              <a class="dropdown-item" @click="loss('Loss')">
+                <i
+                  class="fas fa-level-down-alt fa-sm fa-fw mr-2 text-gray-400"
+                ></i>
+                Loss
+              </a>
+
+              <a class="dropdown-item" @click="open('Open')">
+                <i
+                  class="fas fa-money-check fa-sm fa-fw mr-2 text-gray-400"
+                ></i>
+                Open
+              </a>
+
+              <div class="dropdown-divider"></div>
+              <a
+                class="dropdown-item"
+                @click="recent(1, 'Last 1 Day')"
+                data-toggle="modal"
+                data-target="#uploadModal"
+              >
+                <i class="fas fa-calendar fa-sm fa-fw mr-2 text-gray-400"></i>
+                Last 1 Day
+              </a>
+              <a
+                class="dropdown-item"
+                @click="recent(3, 'Last 3 Days')"
+                data-toggle="modal"
+                data-target="#uploadModal"
+              >
+                <i class="fas fa-calendar fa-sm fa-fw mr-2 text-gray-400"></i>
+                Last 3 Days
+              </a>
+              <a
+                class="dropdown-item"
+                @click="recent(7, 'Last 7 Days')"
+                data-toggle="modal"
+                data-target="#uploadModal"
+              >
+                <i class="fas fa-calendar fa-sm fa-fw mr-2 text-gray-400"></i>
+                Last 15 Days
+              </a>
+              <a
+                class="dropdown-item"
+                @click="recent(7, 'Last 15 Days')"
+                data-toggle="modal"
+                data-target="#uploadModal"
+              >
+                <i class="fas fa-calendar fa-sm fa-fw mr-2 text-gray-400"></i>
+                Last 15 Days
+              </a>
+              <a
+                class="dropdown-item"
+                @click="recent(30, 'Last 30 Days')"
+                data-toggle="modal"
+                data-target="#uploadModal"
+              >
+                <i class="fas fa-calendar fa-sm fa-fw mr-2 text-gray-400"></i>
+                Last 30 Days
+              </a>
+            </div>
+          </span>
+        </div>
+        <div class="row">
+          <div
+            class="col-xl-2 col-md-6 mb-4"
+            v-for="stock in stocks"
+            :key="stock._id"
+          >
+            <div
+              class="card shadow h-100 py-2"
+              :class="
+                stock.total >= 0 ? 'border-left-success' : 'border-left-danger'
+              "
+            >
+              <!-- <router-link :to="{ name: 'Tables', params: { id: 'fb' } }"> -->
+              <router-link :to="{ name: 'Tables', params: { id: stock._id } }">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div
+                        class="text-xs font-weight-bold text-uppercase mb-1"
+                        :class="
+                          stock.total >= 0 ? 'text-success' : 'text-danger'
+                        "
+                      >
+                        ${{ stock.total }}
+                      </div>
+                      <div
+                        class="h7 mb-0 font-weight-bold"
+                        :class="
+                          stock.total >= 0 ? 'text-success' : 'text-danger'
+                        "
+                      >
+                        {{ stock._id }}
+                      </div>
+                    </div>
+                    <!-- <div class="col-auto">
                   <i
                     class="fas fa-dollar-sign fa-2x"
                     :class="stock.total > 0 ? 'text-success' : 'text-danger'"
                   ></i>
                 </div> -->
-              </div>
+                  </div>
+                </div>
+              </router-link>
+              <!-- </router-link> -->
             </div>
-          </router-link>
-          <!-- </router-link> -->
+          </div>
         </div>
       </div>
     </div>
