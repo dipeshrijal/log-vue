b4cffaa95c8fae2fd841cc10859bd8f99047e430
Dipesh Rijal <dipesh.rijal@gmail.com>
2021-01-04 20:08:49 -0600

    watchlist

--- src/store/modules/watchlist.js
+++ src/store/modules/watchlist.js
@@ -1,32 +1,35 @@
-
-import axios from 'axios'
+import axios from "axios";
 
 const state = {
-    watchlist: []
-}
+  watchlist: [],
+};
 
 const actions = {
-    async index({ commit, rootState }) {
-        let watchlist = await axios.get(`${rootState.baseUrl}watchlist`);
+  async index({ commit, rootState }) {
+    let watchlist = await axios.get(`${rootState.baseUrl}watchlist`);
 
-        commit("index", watchlist.data)
-    },
-}
+    commit("index", watchlist.data);
+  },
 
+  async create({ rootState, dispatch }, watchlistForm) {
+    await axios.post(`${rootState.baseUrl}watchlist`, watchlistForm);
+
+    dispatch("index");
+  },
+};
 
 const mutations = {
-    index(state, watchlist) {
-        state.watchlist = watchlist
-    }
-}
+  index(state, watchlist) {
+    state.watchlist = watchlist;
+  },
+};
 
-const getters = {
-}
+const getters = {};
 
 export default {
-    namespaced: true,
-    state,
-    mutations,
-    actions,
-    getters,
-}
+  namespaced: true,
+  state,
+  mutations,
+  actions,
+  getters,
+};
--- src/views/Watchlist.vue
+++ src/views/Watchlist.vue
@@ -2,13 +2,81 @@
   <div class="container-fluid">
     <!-- Page Heading -->
     <div class="row">
-      <div class="col-md-2">
-      </div>
+      <div class="col-md-2"></div>
       <div class="col-md-10">
         <div class="d-sm-flex align-items-center justify-content-between mb-4">
           <h1 class="h4 mb-0 text-gray-800">
+            Watchlist
             <!-- Tickers ({{ totalStocksCount }}) -->
           </h1>
+          <a
+            class="d-none d-sm-inline-block float-end btn btn-lg btn-success shadow-sm"
+            id="filterDropdown"
+            @click="toggleWatchlistModal"
+            role="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+            ><i class="fas fa-plus fa-sm text-white-50"></i>
+          </a>
+
+          <div
+            class="modal fade"
+            :class="watchlistModal ? 'show block' : ''"
+            id="watchlistModal"
+            tabindex="-1"
+            role="dialog"
+            aria-labelledby="exampleModalLabel"
+            aria-hidden="true"
+          >
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">
+                    Create Watchlist
+                  </h5>
+                  <button
+                    class="close"
+                    type="button"
+                    data-dismiss="modal"
+                    aria-label="Close"
+                    @click="toggleWatchlistModal"
+                  >
+                    <span aria-hidden="true">×</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <form
+                    class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search"
+                  >
+                    <div class="input-group">
+                      <input
+                        type="text"
+                        v-model="watchlistForm.ticker"
+                        class="form-control bg-light border-0 small"
+                        placeholder="Enter Name..."
+                        aria-label="Search"
+                        aria-describedby="basic-addon2"
+                      />
+                    </div>
+                  </form>
+                </div>
+                <div class="modal-footer">
+                  <button
+                    class="btn btn-danger"
+                    type="button"
+                    data-dismiss="modal"
+                    @click="toggleWatchlistModal"
+                  >
+                    Cancel
+                  </button>
+                  <button class="btn btn-success" @click="submit">
+                    Submit
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         <div class="row">
           <div
@@ -16,21 +84,18 @@
             v-for="w in watchlist"
             :key="w._id"
           >
-            <div class="card shadow h-100 py-2"
-            >
-              <router-link :to="{ name: 'StockDetails', params: { id: w.ticker } }">
+            <div class="card shadow h-100 py-2">
+              <router-link
+                :to="{ name: 'StockDetails', params: { id: w.ticker } }"
+              >
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
                       <div
                         class="text-xs font-weight-bold text-uppercase mb-1"
-                      >
-                      
-                      </div>
-                      <div
-                        class="h7 mb-0 font-weight-bold"
-                      >
-                      {{w.ticker}}
+                      ></div>
+                      <div class="h7 mb-0 font-weight-bold">
+                        {{ w.ticker }}
                       </div>
                     </div>
                   </div>
@@ -54,7 +119,7 @@
 </template>
 
 <script>
-import { computed } from "vue";
+import { computed, reactive, ref } from "vue";
 import { useStore } from "vuex";
 // import VPagination from "vue3-pagination";
 
@@ -67,6 +132,9 @@ export default {
   // },
   setup() {
     const store = useStore();
+    const watchlistForm = reactive({
+      ticker: "",
+    });
     // const page = ref(store.state.stocks.paginationCurrentPage);
 
     // store.dispatch("watchlist/index", page.value);
@@ -74,7 +142,21 @@ export default {
 
     return {
       // page,
+      watchlistForm,
       watchlist: computed(() => store.state.watchlist.watchlist),
+      watchlistModal: ref(false),
+
+      toggleWatchlistModal() {
+        this.watchlistModal = !this.watchlistModal;
+        this.watchlistForm.ticker = "";
+      },
+
+      submit() {
+        // console.log(this.watchlistForm);
+        store.dispatch("watchlist/create", this.watchlistForm);
+        this.watchlistForm.ticker = "";
+        this.watchlistModal = false;
+      },
       // totalStocksCount: computed(() => store.state.stocks.count),
       // pages: computed(() => Math.ceil(store.state.stocks.count / 30)),
 
@@ -91,5 +173,8 @@ export default {
 a {
   text-decoration: none;
   cursor: pointer;
+}
+.block {
+  display: block;
 }
 </style>
